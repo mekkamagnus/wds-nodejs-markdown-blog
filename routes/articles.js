f16966d91e880c743ac14c23bd304080090cda0a
Mekael Turner <mistnpeace@gmail.com>
2021-11-23 01:11:41 +0800

    Day 1 Commit
    still have to put a lot of functionality in. More tomorrow.

--- routes/articles.js
+++ routes/articles.js
@@ -3,9 +3,12 @@ const router = express.Router();
 // Put all the routes directly related to our article
 
 // Show
-router.get("/", (req, res) => {
-  res.send("In articles");
+router.get("/new", (req, res) => {
+  res.render("articles/new");
 });
+
+// Handle POST logic from new.ejs
+router.post("/", (req, res) => {});
 
 // edit
 
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 // Create server
 const express = require("express");
+// Require mongoose library
+const mongoose = require("mongoose");
 // Require our router
 const articleRouter = require("./routes/articles.js");
 // Get app variable
 const app = express();
 
-// Print out html
+// Render out html
+// Pages are created and managed through routes and a mongodb database
+
+// Connect to mongo database
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Set view engine to ejs
 app.set("view engine", "ejs");
 
 app.use("/articles", articleRouter);
 
-// Create route at index
+// Create route for index
 app.get("/", (req, res) => {
   // Render out index.ejs
 
@@ -31,7 +40,7 @@ app.get("/", (req, res) => {
   ];
 
   // Pass text JavaScript variable from Node.js to index.ejs
-  res.render("index", { articles: articles });
+  res.render("articles/index", { articles: articles });
 });
 
 // Create route
